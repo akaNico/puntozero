03a196cf80d2fa4581daa3fdf16ef3c54a586412
Domenico Garozzo <nico.garozzo.ir>
2017-01-15 22:51:09 +0100

    Product Catalog Responsive

--- wp-content/themes/puntozero/taxonomy-product-category.php
+++ wp-content/themes/puntozero/taxonomy-product-category.php
@@ -6,15 +6,45 @@
             <div id="main" class="col-sm-12 col-md-10 col-md-offset-1" role="main">
     			<?php wp_nav_menu( array( 'theme_location' => 'product-main-categories-menu', 'container_class' => 'main_product_categories_menu_class'  ) ); ?>
 
-                <?php
-                    if(findProductCategoryAnchestor('man') != null || findProductCategoryAnchestor('uomo') != null){
-                        wp_nav_menu( array( 'theme_location' => 'product-categories-man-menu', 'container_class' => 'categories_menu_class product-categories-menu'  ) );
-                    }
-                    if(findProductCategoryAnchestor('woman') != null || findProductCategoryAnchestor('donna') != null){
-                        wp_nav_menu( array( 'theme_location' => 'product-categories-woman-menu', 'container_class' => 'categories_menu_class product-categories-menu'  ) );
-                    }
-                ?>
 
+                <div class="row section-product-category-menu">
+                    <nav class="navbar navbar-default navbar-static-top">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-responsive-product-categories">
+                                <span class="sr-only"><?php _e('Navigation', 'puntozero'); ?></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div id="navbar-responsive-product-categories" class="collapse navbar-collapse">
+                            <?php
+                                if(findProductCategoryAnchestor('man') != null || findProductCategoryAnchestor('uomo') != null){
+                                    wp_nav_menu( array(
+                                        'theme_location' => 'product-categories-man-menu',
+                                        //'container_class' => 'categories_menu_class product-categories-menu',
+                                        'menu' => 'main_nav', /* menu name */
+                                        'menu_class' => 'nav navbar-nav',
+                                        'container' => false, /* container class */
+                                        'depth' => 2,
+                                        'walker' => new puntozero_Bootstrap_walker(),
+                                    ));
+                                }
+                                if(findProductCategoryAnchestor('woman') != null || findProductCategoryAnchestor('donna') != null){
+                                    wp_nav_menu( array(
+                                        'theme_location' => 'product-categories-woman-menu',
+                                        //'container_class' => 'categories_menu_class product-categories-menu'
+                                        'menu' => 'main_nav', /* menu name */
+                                        'menu_class' => 'nav navbar-nav',
+                                        'container' => false, /* container class */
+                                        'depth' => 2,
+                                        'walker' => new puntozero_Bootstrap_walker(),
+                                    ) );
+                                }
+                            ?>
+                        </div>
+                    </nav>
+                </div>
     			<div class="clearfix">&nbsp;</div>
     			<div class="container-fluid container-fluid-without-padding">
                     <div class="row <?php echo the_category_unlinked(' '); ?>">
